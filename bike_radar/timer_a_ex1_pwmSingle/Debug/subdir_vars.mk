6582a204e212c9d4a22bd2f83e9a8ca1181c17c6
MichelGeorgesNajarian <migeo.najarian@gmail.com>
2019-07-30 11:23:26 -0400

    final version of code, working prototype

---
+++ bike_radar/timer_a_ex1_pwmSingle/Debug/subdir_vars.mk
@@ -0,0 +1,29 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430f5529.cmd 
+
+C_SRCS += \
+../timer_a_ex1_pwmSingle.c 
+
+C_DEPS += \
+./timer_a_ex1_pwmSingle.d 
+
+OBJS += \
+./timer_a_ex1_pwmSingle.obj 
+
+OBJS__QUOTED += \
+"timer_a_ex1_pwmSingle.obj" 
+
+C_DEPS__QUOTED += \
+"timer_a_ex1_pwmSingle.d" 
+
+C_SRCS__QUOTED += \
+"../timer_a_ex1_pwmSingle.c" 
+
+
---
+++ bike_radar/timer_a_ex1_pwmSingle/Debug/timer_a_ex1_pwmSingle.d
@@ -0,0 +1,123 @@
+# FIXED
+
+timer_a_ex1_pwmSingle.obj: ../timer_a_ex1_pwmSingle.c
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/driverlib.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/inc/hw_memmap.h
+timer_a_ex1_pwmSingle.obj: C:/ti/ccs901/ccs/ccs_base/msp430/include/msp430.h
+timer_a_ex1_pwmSingle.obj: C:/ti/ccs901/ccs/ccs_base/msp430/include/msp430f5529.h
+timer_a_ex1_pwmSingle.obj: C:/ti/ccs901/ccs/ccs_base/msp430/include/in430.h
+timer_a_ex1_pwmSingle.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-msp430_18.12.1.LTS/include/intrinsics.h
+timer_a_ex1_pwmSingle.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-msp430_18.12.1.LTS/include/intrinsics_legacy_undefs.h
+timer_a_ex1_pwmSingle.obj: C:/ti/ccs901/ccs/ccs_base/msp430/include/msp430f5xx_6xxgeneric.h
+timer_a_ex1_pwmSingle.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-msp430_18.12.1.LTS/include/stdint.h
+timer_a_ex1_pwmSingle.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-msp430_18.12.1.LTS/include/_stdint40.h
+timer_a_ex1_pwmSingle.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-msp430_18.12.1.LTS/include/sys/stdint.h
+timer_a_ex1_pwmSingle.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-msp430_18.12.1.LTS/include/sys/cdefs.h
+timer_a_ex1_pwmSingle.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-msp430_18.12.1.LTS/include/sys/_types.h
+timer_a_ex1_pwmSingle.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-msp430_18.12.1.LTS/include/machine/_types.h
+timer_a_ex1_pwmSingle.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-msp430_18.12.1.LTS/include/machine/_stdint.h
+timer_a_ex1_pwmSingle.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-msp430_18.12.1.LTS/include/sys/_stdint.h
+timer_a_ex1_pwmSingle.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-msp430_18.12.1.LTS/include/stdbool.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/sysctl.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/dma.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/eusci_b_i2c.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/rtc_b.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/crc.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/usci_a_uart.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/flashctl.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/ram.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/dac12_a.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/pmap.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/mpy32.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/rtc_a.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/ucs.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/aes.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/tec.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/usci_a_spi.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/sfr.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/wdt_a.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/timer_a.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/oa.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/pmm.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/lcd_b.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/rtc_c.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/adc10_a.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/eusci_a_uart.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/ctsd16.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/usci_b_spi.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/comp_b.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/gpio.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/tlv.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/ldopwr.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/adc12_a.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/ref.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/eusci_a_spi.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/timer_b.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/battbak.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/usci_b_i2c.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/eusci_b_spi.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/timer_d.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/lcd_c.h
+timer_a_ex1_pwmSingle.obj: C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/sd24_b.h
+timer_a_ex1_pwmSingle.obj: C:/ti/ccs901/ccs/ccs_base/msp430/include/msp430fr4133.h
+
+../timer_a_ex1_pwmSingle.c: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/driverlib.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/inc/hw_memmap.h: 
+C:/ti/ccs901/ccs/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccs901/ccs/ccs_base/msp430/include/msp430f5529.h: 
+C:/ti/ccs901/ccs/ccs_base/msp430/include/in430.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-msp430_18.12.1.LTS/include/intrinsics.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-msp430_18.12.1.LTS/include/intrinsics_legacy_undefs.h: 
+C:/ti/ccs901/ccs/ccs_base/msp430/include/msp430f5xx_6xxgeneric.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-msp430_18.12.1.LTS/include/stdint.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-msp430_18.12.1.LTS/include/_stdint40.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-msp430_18.12.1.LTS/include/sys/stdint.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-msp430_18.12.1.LTS/include/sys/cdefs.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-msp430_18.12.1.LTS/include/sys/_types.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-msp430_18.12.1.LTS/include/machine/_types.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-msp430_18.12.1.LTS/include/machine/_stdint.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-msp430_18.12.1.LTS/include/sys/_stdint.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-msp430_18.12.1.LTS/include/stdbool.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/sysctl.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/dma.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/eusci_b_i2c.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/rtc_b.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/crc.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/usci_a_uart.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/flashctl.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/ram.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/dac12_a.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/pmap.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/mpy32.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/rtc_a.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/ucs.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/aes.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/tec.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/usci_a_spi.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/sfr.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/wdt_a.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/timer_a.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/oa.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/pmm.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/lcd_b.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/rtc_c.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/adc10_a.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/eusci_a_uart.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/ctsd16.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/usci_b_spi.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/comp_b.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/gpio.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/tlv.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/ldopwr.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/adc12_a.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/ref.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/eusci_a_spi.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/timer_b.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/battbak.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/usci_b_i2c.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/eusci_b_spi.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/timer_d.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/lcd_c.h: 
+C:/Users/migeo/OneDrive//\ -//\ University//\ of//\ Waterloo/2B/ECE298/bike_radar/timer_a_ex1_pwmSingle/driverlib/MSP430F5xx_6xx/sd24_b.h: 
+C:/ti/ccs901/ccs/ccs_base/msp430/include/msp430fr4133.h: 
